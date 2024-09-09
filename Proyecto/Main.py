3d5de5d7b3edea94488b7623a8288760f31d6022
Rosendo Pardo <rosen.pardox@gmail.com>
2024-09-09 10:37:44 -0300

    Se agrega la carpeta de Proyecto

---
+++ Proyecto/Main.py
@@ -0,0 +1,11 @@
+# import Funciones
+
+# from Funciones import saludar, saludar_profesor
+
+from Paquete.Funciones import *
+from pygame import *
+
+saludar("Hola, 111")
+saludar_profesor("Pepe", "Biología")
+
+print(TAMAÑO_PANTALLA)
